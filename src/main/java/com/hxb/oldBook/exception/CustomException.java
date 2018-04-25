c96c04cea78aee64e96c688abe6f6714c2e8a963
hxb6 <1442467176@qq.com>
2018-04-25 17:03:29 +0800

    自定义异常

---
+++ src/main/java/com/hxb/oldBook/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.hxb.oldBook.exception;
+
+/**
+ * @Package: com.hxb.oldBook.exception
+ * @Author: HeXiaoBo
+ * @CreateDate: 2018/4/25 17:01
+ * @Description: 自定义异常
+ **/
+public class CustomException extends RuntimeException{
+}
