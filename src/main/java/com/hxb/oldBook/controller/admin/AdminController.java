3adf3d040c8a08aef5435380647abc6793544d73
hxb6 <1442467176@qq.com>
2018-05-06 22:13:33 +0800

    申请成为商家 完成三分之二

---
+++ src/main/java/com/hxb/oldBook/controller/admin/AdminController.java
@@ -0,0 +1,10 @@
+package com.hxb.oldBook.controller.admin;
+
+/**
+ * @Package: com.hxb.oldBook.controller.admin
+ * @Author: HeXiaoBo
+ * @CreateDate: 2018/5/6 13:49
+ * @Description:
+ **/
+public class AdminController {
+}
